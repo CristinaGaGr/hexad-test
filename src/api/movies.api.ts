161bc32172a63fe26fb794ce6c04ff0293a75a6b
Cristina Garcia <cristinagarciagrillo@gmail.com>
2020-05-02 00:18:16 +0200

    Add material UI rating stars component

--- src/api/movies.api.ts
+++ src/api/movies.api.ts
@@ -1,6 +1,12 @@
 import { movies } from './movies';
-import {Movie} from "../modules/movies/movie.model";
+import { Movie } from "../modules/movies/movie.model";
 
 export const getMovies = (): Promise<Movie[]> => {
 	return new Promise((resolve) => setTimeout(() => resolve(movies), 200));
+};
+
+export const rateMovie = (id: number, rating: number) => {
+	const movie: any = movies.find((e) => e.id === id);
+	movie.rating = rating;
+	return new Promise((resolve) => setTimeout(() => resolve(movie), 200));
 };
--- src/modules/movies/components/movie/movie.tsx
+++ src/modules/movies/components/movie/movie.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './movie.module.scss';
 import { Movie } from '../../movie.model';
+import { Rating } from '@material-ui/lab';
 
 
 export const MovieComponent = ({title, description, director, year, imgUrl, rating, id}: Movie) => {
+	const [stars, setStars] = useState(rating);
+
 	return (
 		<div className={styles.container}>
 			<img src={imgUrl} alt={'film'}/>
@@ -11,6 +14,13 @@ export const MovieComponent = ({title, description, director, year, imgUrl, rati
 			<p><strong>Director:</strong> {director}</p>
 			<p><strong>Summary:</strong> {description}</p>
 			<p><strong>Release:</strong> {year}</p>
+			<Rating
+				name="simple-controlled"
+				value={stars}
+				onChange={(event: any, newValue: any) => {
+					setStars(newValue);
+				}}
+			/>
 		</div>
 	);
 };
